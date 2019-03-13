60b35b9e9adb3155e376fb1ac86d0afd5bafbc0f
Dongfeng.Liang@Teradata.com <Dongfeng.Liang@Teradata.com>
2019-03-13 10:37:25 +0800

    修改Task任务

--- src/main/java/com/df/datax/task/JobTask.java
+++ src/main/java/com/df/datax/task/JobTask.java
@@ -1,6 +1,11 @@
 package com.df.datax.task;
+import com.df.datax.service.DataxService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 利用spring定时任务调起job
@@ -8,9 +13,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class JobTask {
 
+    @Autowired
+    private DataxService dataxService;
 //    每天中午12点触发
     @Scheduled(cron = "0 0 12 * * ?")
     public void taskTeradataJob() {
+        Map params=new HashMap(16);
+//        放开即可调度作业
+//        dataxService.exeDatax("teradatatomysql",params);
         System.out.println("每天中午12点触发配置定时任务");
     }
 //    上午10:15触发
